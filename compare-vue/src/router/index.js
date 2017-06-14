9c7ef237f1aebae5077280f5bf85fda519f61f95
ysk8 <>
2017-06-13 23:58:44 -0500

    Added the first Vue components, Home and Faq

--- compare-vue/src/router/index.js
+++ compare-vue/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Home from '@/components/Home'
+import Faq from '@/components/Faq'
 
 Vue.use(Router)
 
@@ -9,7 +10,12 @@ export default new Router({
     {
       path: '/',
       name: 'Hello',
-      component: Hello
+      component: Home
+    },
+    {
+      path: '/faq',
+      name: 'Faq',
+      component: Faq
     }
   ]
 })
