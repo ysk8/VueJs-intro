dc6ee38860273c08d78f8c1ea720998ca63c89cf
ysk8 <gyesidr2sk8@gmail.com>
2017-07-14 00:30:42 -0500

    Added header template

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="nav has-shadow">
+      <div class="container">
+        <div class="nav-left">
+          <a class="nav-item">My Company</a>
+        </div>
+        <span class="nav-toggle">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="nav-right nav-menu">
+
+          <router-link to="/" class="nav-item r-item">Home</router-link>
+          <router-link to="faq" class="nav-item r-item">Feautures</router-link>
+          <router-link to="faq" class="nav-item r-item">About</router-link>
+          <router-link to="faq" class="nav-item r-item">FAQ</router-link>
+
+          <div class="nav-item">
+            <p class="control">
+              <a class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>Join Now</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
