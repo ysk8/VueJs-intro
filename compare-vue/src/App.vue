d82a6879ffa86c3b6e323d280a381aab85bf6446
ysk8 <gyesidr2sk8@gmail.com>
2017-07-19 02:00:27 -0500

    Added directives to burger menu. Fixes on mq file

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -5,13 +5,14 @@
         <div class="nav-left">
           <a class="nav-item">My Company</a>
         </div>
-        <span class="nav-toggle">
+
+        <span class="nav-toggle" v-on:click="toggleNav" v-bind:class="{ 'is-active': isActive }">
           <span></span>
           <span></span>
           <span></span>
         </span>
 
-        <div class="nav-right nav-menu">
+        <div class="nav-right nav-menu" v-bind:class="{ 'is-active': isActive }">
 
           <router-link to="/" class="nav-item r-item">Home</router-link>
           <router-link to="faq" class="nav-item r-item">Feautures</router-link>
@@ -37,7 +38,17 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data: function() {
+    return {
+       isActive : false
+    }   
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive = !this.isActive
+    }
+  }
 }
 </script>
 
@@ -50,16 +61,19 @@ export default {
   a:hover
     color: gray
 
-  .nav-left a
-    color: #fff
-    font-weight: bold
+.nav-left a
+  color: #fff
+  font-weight: bold
 
-  a.r-item
-    color: #C1C1C1
-    padding: 0.5rem 1.75rem
-    +mobile
-      color: gray
-      &:hover
-        background-color: #F1F1F1
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile 
+    color: gray !important
+    &:hover
+      background-color: #F1F1F1 !important
+
+.nav-toggle span
+  background-color: #C1C1C1
 
 </style>
